1123b640676a9c74255144c8af799662c37aef78
unknown <eternalpilgrim8@gmail.com>
2020-05-07 16:33:22 +0300

    Добавлена проверка что по ссылке логина действительно открывается страница логина

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from  .pages.main_page import MainPage
+from  .pages.login_page import LoginPage
 
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
@@ -9,6 +10,11 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser,link)
     page.open()
     page.go_to_login_page()
+
+    # проверяем, что открылась действительно страница логина
+    page_login = LoginPage(browser,browser.current_url)
+    page_login.should_be_login_page()
+
     
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
